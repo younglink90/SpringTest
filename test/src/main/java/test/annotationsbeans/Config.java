dc534ef190c8ef1cc295ce7dd4090098f1bf7b6a
Jordi <ynglnk90@gmail.com>
2016-01-27 20:28:35 +0100

    identation

--- test/src/main/java/test/annotationsbeans/Config.java
+++ test/src/main/java/test/annotationsbeans/Config.java
@@ -14,34 +14,34 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 @Configuration
-public class Config
-{
+public class Config {
+	
 	@Autowired
 	ApplicationContext myContext;
-	
-    @Bean(name="car1") 
-    public Car createCar1() {
-    	return new Car("PK","Red");
-    }
-    
-    @Bean(name="car2") 
-    public Car createCar2() {
-    	return new Car("Nightwish","Violet");
-    }
-    
-    @Bean(name="car3") 
-    public Car createCar3() {
-    	return new Car("LOLIPOPY","Black");
-    }
-    
-    @Bean(name="myParking")
-    public Parking getParking() {
-    	return  new Parking();
-    }
-    
-    @Bean(name="carList")
-    public List<Car> createCarList() {
-     	Map<String, Car> carMap = myContext.getBeansOfType(Car.class);
-    	return new ArrayList<>(carMap.values());
-    }
+
+	@Bean(name = "car1")
+	public Car createCar1() {
+		return new Car("PK", "Red");
+	}
+
+	@Bean(name = "car2")
+	public Car createCar2() {
+		return new Car("Nightwish", "Violet");
+	}
+
+	@Bean(name = "car3")
+	public Car createCar3() {
+		return new Car("LOLIPOPY", "Black");
+	}
+
+	@Bean(name = "myParking")
+	public Parking getParking() {
+		return new Parking();
+	}
+
+	@Bean(name = "carList")
+	public List<Car> createCarList() {
+		Map<String, Car> carMap = myContext.getBeansOfType(Car.class);
+		return new ArrayList<>(carMap.values());
+	}
 }
